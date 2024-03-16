7c03355f73544ceb515f019366e376527db0eebf
Rakesh-Molla <rakesh2020molla@gmail.com>
2024-03-16 22:25:51 +0530

    Dashbor components added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,6 @@
-import {Sidebar} from "./_components/sidebar";
+import { Navbar } from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+
 
 const DashboardLayout = ({
     children
@@ -7,10 +9,15 @@ const DashboardLayout = ({
 }) => {
     return ( 
         <div className="h-full">
+            <div className="h-[80px] md:pl-56 flxed inset-y-0 w-full z-50">
+                <Navbar />
+            </div>
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
             <Sidebar />
             </div>
+            <main className="md:pl-56 pt-[80px] h-full">
             {children}
+            </main>
         </div>
      );
 }
